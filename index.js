7908d1ab9c98d4239a9f08db50ff92bcb81decb8
Ting Zhou <limtingzhou@gmail.com>
2018-12-22 16:06:12 +0800

    Use request package to send http request
    http request is sent to convert quantity of cryptocurrency to currency.

--- index.js
+++ index.js
@@ -17,16 +17,29 @@ app.get("/", function(req, res) {
 
 
 app.post("/",function(req, res) {
+
   let crypto = req.body.crypto;
   let fiat = req.body.fiat;
+  let amount = req.body.amount;
   let symbol_set = "global";
-  let symbol = crypto + fiat;
-  console.log("Crypto: " + crypto);
-  console.log("Fiat: " + fiat);
+  let baseUrl = `https://apiv2.bitcoinaverage.com/convert/global`;
+  let options = {
+    url: baseUrl,
+    method: "GET",
+    qs: {
+      from: crypto,
+      to: fiat,
+      amount: amount
+    }
+  };
 
-  let link = `https://apiv2.bitcoinaverage.com/indices/${symbol_set}/ticker/${symbol}`;
-  console.log(link);
-  request(link, function(error, response, body) {
-    console.log(body);
+  request(options, function(error, response, body) {
+    let data = JSON.parse(body);
+    let price = data.price;
+    let currentDate = data.time;
+
+    res.write(`<p>The current date is ${currentDate}<p>`);
+    res.write(`<h1>The price of ${amount} ${crypto} is ${price} ${fiat}</h1>`);
+    res.send();
   });
 });
